77f3096a2fc1869967bf4aac6195ad98c84bef90
frotunato <frotunato@gmail.com>
2020-03-25 18:24:13 +0100

    fixed puppeteer

--- instagram.js
+++ instagram.js
@@ -6,7 +6,7 @@ const mobile = devices['iPhone XR'];
 const path = require('path');
 
 async function post (description, imgPath, cb) {
-  const browser = await puppeteer.launch({headless: true});
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.emulate(mobile);
   try {
